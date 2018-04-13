091af1fd97d6c18c8ff7780709158da70db5b83c
gibranestrada <gibran9@gmail.com>
2018-04-13 17:13:33 -0400

    map() phone data

--- app.js
+++ app.js
@@ -13,35 +13,33 @@ let phones;
     })
     .catch(function(error){
         console.log(error);
+        alert("Refresh the page");
     });
-    
-
-/* const phone = async () => {
-    const response = await fetch('http://www.mocky.io/v2/5acfc4df3100006a004eaab4');
-    const json = await return response.json();
-    
-    console.log(json);
-} */
 
 
 //variables
     const phoneCard = document.querySelector('#card');
+    let phoneArray;
 
-//load function
-setTimeout(loadFunction, 1000);
+    //load function
+setTimeout(loadFunction, 700);
 
 //functions
 function loadFunction(){
-    phoneCard.innerHTML = `
-    
-        <img src="${phones[0].image}" class='phone-image'>
+    phoneArray = phones.map((listing, index) => {
+        return listing =
+
+     `<div class='card' key = ${index}>
+        <img src="${listing.image}" class='phone-image' style="width: 100%"><hr>
         <div class='info-card'>
-            <h4>Apple iPhone 5c</h4>
-            <p>${phones[0].description}</p>
-            <img src=''>
+            <h4>${listing.name}</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nul..</p>
             <p class='price'>$400 <span>$300</span></p>
             <a href='#' class='button primary button input add-to-cart' data-id='1'>Add to Cart</a>
+            <a href='#' class='button primary button input add-to-cart' data-id='1'>More info</a>
         </div>
-        
-    `
+        </div><br/>
+    ` 
+    }).join("")
+    phoneCard.innerHTML = phoneArray;
 } 
