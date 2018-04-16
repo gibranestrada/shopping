c13f3812c12ac0c30aa88a670d28b7b68734c284
gibranestrada <gibran9@gmail.com>
2018-04-16 16:56:01 -0400

    changes to hidden card

--- app.js
+++ app.js
@@ -20,12 +20,13 @@ let phones;
 //variables
     const phoneCard = document.querySelector('#card');
     let phoneArray,
-    phoneArray2,
+        phoneArray2,
         infoButton,
-        cardsButton;
+        cardsButton,
+        deleteButton;
 
     //load listeners
-//setTimeout(loadFunction, 400);
+setTimeout(loadFunction, 400);
 setTimeout(loadFunction, 500);
 
 setTimeout(loadEventListeners, 700);
@@ -35,12 +36,14 @@ setTimeout(loadEventListeners, 700);
 function loadEventListeners(){
     infoButton = document.querySelectorAll(`.more-info`);
     cardsButton = document.querySelector('#shopping-car2');
+   
+
 }
 
-setTimeout(info, 1500);
+setTimeout(info, 900);
 function info(){
     infoButton.forEach(aClass => aClass.addEventListener('click', showInfoButton));
-    console.log(infoButton)
+    
 }
 //functions
 function loadFunction(){
@@ -56,9 +59,9 @@ function loadFunction(){
             <div class= 'visible-card'>
             <ul>
                 <li class = 'submenu-card'>
-                    <a class='more-info' id='data-${listing.id}' data-id='${listing.id}'>More info</a>
-                    <div id='shopping-car${listing.id}'>
-                    </div>
+                    <a class='more-info' id='data-${listing.id}' data-id='${listing.id}' 
+                    >More info</a>
+                    
                 </li>
             </ul>
             </div>
@@ -68,16 +71,35 @@ function loadFunction(){
     }).join("")
   
     phoneCard.innerHTML = phoneArray;
+   // openCards();
 } 
+/*
+let openButton;
+function openCards(){
+    openButton = document.querySelectorAll(".more-info");
+    openButton.forEach(aClass => aClass.addEventListener('click', openCard));
+}
+function openCard(e){
+    openButton = document.getElementById(e.target.id)
+    openButton.style.display = "block";
+}
+
+function closeCard(e){
+    let closeButton = document.querySelector(".extra-info");
+    closeButton.style.display = "none";
+} */
+
 
 function showInfoButton(e){
-    cardsButton = document.getElementById(event.target.id);
-    console.log(cardsButton)
+    cardsButton = document.getElementById(e.target.id);
     e.preventDefault();
+    let cardDesc = document.createElement('div');
+    cardDesc.classList.add('hello');
     phoneArray = phones.map((listing,id)=> {
         return listing =
         `<div class='extra-info' id='data-${listing.id}'>
         <img src='${listing.image}' id='card-image' style='width:150px; height:150px'>
+        <a href='#' class='close-button' id='close-button'><b>X</b></a>
             <table>
                 <thead>
                     <tr>
@@ -111,15 +133,19 @@ function showInfoButton(e){
     }else  if(event.target.id == 'data-6' &&  phoneArray[5].includes('data-6') == true){
         phoneArray = phoneArray[5]
     }
-    //cardsButton.innerHTML = phoneArray.find(function(){
-     //   return e.target.id
-    //})
-    cardsButton.innerHTML = phoneArray;
+    cardDesc.innerHTML = phoneArray;
+    cardsButton.appendChild(cardDesc);
+    buttonDeleter();
 }
-
-
-
-
+function buttonDeleter(){
+    deleteButton = document.querySelectorAll('.close-button');
+    deleteButton.forEach(deleteBtn => deleteBtn.addEventListener('click', deleteCard));
+}
+function deleteCard(e){
+    e.preventDefault;
+    list = document.querySelector('.hello');
+    cardsButton.removeChild(list);
+} 
 
 
 
