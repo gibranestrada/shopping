30cfcec3c66d50cf3300ef9df63b3d16593cac4f
gibranestrada <gibran9@gmail.com>
2018-04-16 21:21:47 -0400

    api fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 //https://www.mocky.io/v2/5ad5471a3200006600202a94 works on github
 //https://www.mocky.io/v2/5acfc4df3100006a004eaab4
 let phones;
-    fetch('https://www.mocky.io/v2/5ad5471a3200006600202a94')
+    fetch('https://www.mocky.io/v2/5ad54c103200005800202a9c')
     .then(response => {
         return response.json();
     })
